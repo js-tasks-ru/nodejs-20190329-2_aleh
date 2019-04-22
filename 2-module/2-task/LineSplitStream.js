1bd88ac7f43fa67353e48450c418c9ac032e3a1b
Oleg Babashko <oleg.babashko@synesis.ru>
2019-04-23 02:27:08 +0300

    2-module: 2-task: LineSplitStream: split all incoming messages by `os.EOL` symbol

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -1,16 +1,45 @@
 const stream = require('stream');
-const os = require('os');
+const { EOL } = require('os');
 
 class LineSplitStream extends stream.Transform {
-  constructor(options) {
-    super(options);
-  }
-  
-  _transform(chunk, encoding, callback) {
-  }
-  
-  _flush(callback) {
-  }
+    constructor(options) {
+        super(options);
+        // buffer to save last part of data
+        this.currentBuffer = [];
+    }
+
+    _transform(chunk, encoding, callback) {
+        // split all chunk data by `EOL` symbol
+        const messagesList = chunk.toString().split(EOL);
+
+        // TODO: Do we need this block?
+        if (messagesList.length < 1) {
+            callback();
+            return;
+        }
+        // push first part of data to current buffer and remove it from messages list
+        this.currentBuffer.push(messagesList.shift());
+
+        // if new message had EOL symbol = send all buffered data
+        if (messagesList.length) {
+            this._sendAndEmptyCurrentBuffer();
+            this.currentBuffer.push(messagesList.pop());
+            messagesList.forEach((message) => this.push(message));
+        }
+        callback();
+    }
+
+    _flush(callback) {
+        if (this.currentBuffer.length) {
+            this._sendAndEmptyCurrentBuffer();
+        }
+        callback();
+    }
+
+    _sendAndEmptyCurrentBuffer() {
+        this.push(this.currentBuffer.join(''))
+        this.currentBuffer.length = 0;
+    }
 }
 
 module.exports = LineSplitStream;
