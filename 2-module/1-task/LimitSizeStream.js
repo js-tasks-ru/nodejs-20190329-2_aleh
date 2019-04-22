8ccad68e8dcaa96460bd69d3dec04f2c144256dd
Oleg Babashko <oleg.babashko@synesis.ru>
2019-04-22 04:31:38 +0300

    2-module: 1-task: implement LimitSizeStream with data limit

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -4,10 +4,19 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    const { limit = 0, objectMode = false } = options;
+    this.isObjectMode = objectMode;
+    this.limit = limit;
+    this.size = 0;
   }
 
   _transform(chunk, encoding, callback) {
-
+    this.size += this.isObjectMode ? 1 : chunk.toString().length;
+    if (this.size > this.limit) {
+      callback(new LimitExceededError());
+    } else {
+      callback(null, chunk);
+    }
   }
 }
 
