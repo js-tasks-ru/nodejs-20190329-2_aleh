d16a50da81c4fb16d5a716b6afd1bb6fa9a8f52b
Oleg Babashko <oleg.babashko@synesis.ru>
2019-04-23 04:01:04 +0300

    4-module: 1-task: implement GET file

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,23 +1,53 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const stream = require('stream');
+
+const handleError = require('./error-handler');
 
 const server = new http.Server();
 
+const FilesDirName = path.join(__dirname, 'files');
+
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
-  const filepath = path.join(__dirname, 'files', pathname);
+  const filepath = path.join(FilesDirName, pathname);
 
   switch (req.method) {
     case 'GET':
-
+      getFileSafe(filepath, res);
       break;
 
     default:
-      res.statusCode = 501;
-      res.end('Not implemented');
+      handleError(501, res);
   }
 });
 
 module.exports = server;
+
+function getFileSafe(filepath, res) {
+    if (~filepath.indexOf('\0')) {
+        handleError(500, res);
+        return;
+    }
+    if (path.dirname(filepath) !== FilesDirName) {
+        handleError(400, res);
+        return;
+    }
+    if (!fs.existsSync(filepath)) {
+        handleError(404, res);
+        return;
+    }
+    getFile(filepath, res);
+}
+
+function getFile(filepath, res) {
+    const file = fs.createReadStream(filepath);
+    stream.pipeline(file, res, (err) => {
+        if (err) {
+            handleError(500, res);
+            return;
+        }
+    });
+}
