cbec07074f69abd49e0bb85318772414ad4805e9
Oleg Babashko <oleg.babashko@synesis.ru>
2020-04-01 09:24:04 +0300

    4-module: 3-task: Resolve DELETE file task; refactor previous 4-module 2-task

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -11,7 +12,17 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'DELETE':
-
+      if (pathHasNestedFolders(pathname)) {
+        res.statusCode = 400;
+        res.end('Nested folders are not supported!');
+        return;
+      }
+      if (!fs.existsSync(filepath)) {
+        res.statusCode = 404;
+        res.end('File not found!');
+        return;
+      }
+      deleteFile(filepath, res);
       break;
 
     default:
@@ -21,3 +32,26 @@ server.on('request', (req, res) => {
 });
 
 module.exports = server;
+
+function pathHasNestedFolders(filepath) {
+  return /\/|\.\.|\\/g.test(filepath);
+}
+
+function deleteFile(filepath, res) {
+  try {
+    fs.unlink(filepath, (err) => {
+      if (err) {
+        internalServerError();
+      } else {
+        res.end('SUCCESS!');
+      }
+    })
+  } catch (e) {
+    internalServerError(res);
+  }
+}
+
+function internalServerError(res) {
+  res.statusCode = 500;
+  res.end('Internal server error.');
+}
