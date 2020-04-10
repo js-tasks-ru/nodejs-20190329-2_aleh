48ec1508fa1cf7a415a4aefdddc3712f44fc923e
Oleg Babashko <oleg.babashko@synesis.ru>
2020-04-10 09:30:28 +0300

    6-module: 1-task: add mongoose schema for User model

--- 6-module/1-task/User.js
+++ 6-module/1-task/User.js
@@ -1,5 +1,29 @@
 const mongoose = require('mongoose');
 
-const schema = new mongoose.Schema({});
+const schema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        // match: /^[-.\w]+@([\w-]+\.)+[\w-]{2,12}$/,
+        validate: [{
+            validator: function (email) {
+                const regexp = /^[-.\w]+@([\w-]+\.)+[\w-]{2,12}$/;
+                return regexp.test(email);
+            },
+            msg: 'Email should match pattern!',
+        }],
+        lowercase: true,
+        trim: true,
+    },
+    displayName: {
+        type: String,
+        required: true,
+        index: true,
+        trim: true,
+    },
+}, {
+    timestamps: true,
+});
 
 module.exports = mongoose.model('User', schema);
