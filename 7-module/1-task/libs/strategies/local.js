0c1355762f0dc93319b4455e8e29bef681f64e22
Oleg Babashko <oleg.babashko@synesis.ru>
2020-04-12 10:13:20 +0300

    7-module: 1-task: change async function

--- 7-module/1-task/libs/strategies/local.js
+++ 7-module/1-task/libs/strategies/local.js
@@ -3,21 +3,23 @@ const User = require('../../models/User');
 
 module.exports = new LocalStrategy(
     {usernameField: 'email', session: false},
-    async function(email, password, done) {
-        try {
-            const user = await User.findOne({ email });
-            if (!user) {
-                done(null, false, 'Нет такого пользователя');
-                return;
+    function(email, password, done) {
+        (async () => {
+            try {
+                const user = await User.findOne({ email });
+                if (!user) {
+                    done(null, false, 'Нет такого пользователя');
+                    return;
+                }
+                const isPasswordValid = await user.checkPassword(password);
+                if (!isPasswordValid) {
+                    done(null, false, 'Невереный пароль');
+                } else {
+                    done(null, user);
+                }
+            } catch (e) {
+                done(e, false, 'Ошибка сервера при проверке пароля');
             }
-            const isPasswordValid = await user.checkPassword(password);
-            if (!isPasswordValid) {
-                done(null, false, 'Невереный пароль');
-            } else {
-                done(null, user);
-            }
-        } catch (e) {
-            done(e, false, 'Ошибка сервера при проверке пароля');
-        }
+        })()
     }
 );
