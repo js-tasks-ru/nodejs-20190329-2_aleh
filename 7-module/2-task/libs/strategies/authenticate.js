25e9378931729da8764f55a0f49dbe3c95454a8f
Oleg Babashko <babashkoov@gmail.com>
2020-04-12 14:42:21 +0300

    7-module: 2-task: authenticate helper to work with db

--- 7-module/2-task/libs/strategies/authenticate.js
+++ 7-module/2-task/libs/strategies/authenticate.js
@@ -1,3 +1,17 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+const UserModel = require('../../models/User');
+
+module.exports = async function authenticate(strategy, email, displayName, done) {
+  try {
+    if (!email || !displayName) {
+      done(null, false, 'Не указан email');
+      return;
+    }
+    let user = await UserModel.findOne({ email });
+    if (!user) {
+      user = await UserModel.create({ email, displayName });
+    }
+    done(null, user);
+  } catch (e) {
+    done(e, false, 'Ошибка при создании пользователя');
+  }
 };
